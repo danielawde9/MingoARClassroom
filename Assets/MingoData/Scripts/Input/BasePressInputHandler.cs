e3868202af87900ced0285ef0165389fc3e8b891
Daniel Awde <danielawde9@gmail.com>
2023-05-13 15:07:44 +0300

    Update BasePressInputHandler.cs

--- Assets/MingoData/Scripts/Input/BasePressInputHandler.cs
+++ Assets/MingoData/Scripts/Input/BasePressInputHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public abstract class BasePressInputHandler : MonoBehaviour
 {
     protected InputAction m_PressAction;
+    protected InputAction m_DragAction;
 
     protected virtual void Awake()
     {
@@ -14,6 +15,7 @@ public abstract class BasePressInputHandler : MonoBehaviour
             if (ctx.control.device is Pointer device)
             {
                 OnPressBegan(device.position.ReadValue());
+                m_DragAction.Enable(); // Start dragging when pressing starts
             }
         };
 
@@ -25,8 +27,31 @@ public abstract class BasePressInputHandler : MonoBehaviour
             }
         };
 
-        m_PressAction.canceled += _ => OnPressCancel();
+        m_PressAction.canceled += _ =>
+        {
+            OnPressCancel();
+            m_DragAction.Disable(); // Stop dragging when pressing is cancelled
+        };
+
+        m_DragAction = new InputAction("drag", InputActionType.PassThrough);
+        m_DragAction.AddCompositeBinding("Dpad")
+            .With("Up", "<Pointer>/delta/y")
+            .With("Down", "<Pointer>/delta/-y") // Negative Y delta for Down
+            .With("Left", "<Pointer>/delta/-x") // Negative X delta for Left
+            .With("Right", "<Pointer>/delta/x");
+
+        m_DragAction.performed += ctx =>
+        {
+            if (ctx.control.device is Pointer device)
+            {
+                OnDrag(device.delta.ReadValue());
+            }
+        };
+
+        m_DragAction.canceled += _ => OnDragEnd();
     }
+
+    protected virtual void OnDrag(Vector2 delta) { }
 
     protected virtual void OnEnable()
     {
@@ -36,11 +61,15 @@ public abstract class BasePressInputHandler : MonoBehaviour
     protected virtual void OnDisable()
     {
         m_PressAction.Disable();
+        m_DragAction.Disable();
     }
+
+    protected virtual void OnDragEnd() { }
 
     protected virtual void OnDestroy()
     {
         m_PressAction.Dispose();
+        m_DragAction.Dispose();
     }
 
     protected virtual void OnPress(Vector3 position) { }
